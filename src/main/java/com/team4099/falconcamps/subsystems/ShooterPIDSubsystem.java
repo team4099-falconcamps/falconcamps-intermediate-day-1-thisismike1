b24886a83dc01dbb0f7871c6be7ba0eb65592685
thisismike1 <molm8031@chsphantoms.org>
2020-07-15 18:24:55 +0000

    Log

--- src/main/java/com/team4099/falconcamps/subsystems/ShooterPIDSubsystem.java
+++ src/main/java/com/team4099/falconcamps/subsystems/ShooterPIDSubsystem.java
@@ -5,9 +5,10 @@ import com.revrobotics.*;
 import edu.wpi.first.wpilibj.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.PIDSubsystem;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import io.github.oblarg.oblog.Loggable;
 import io.github.oblarg.oblog.annotations.Log;
 
-public class ShooterPIDSubsystem extends PIDSubsystem {
+public class ShooterPIDSubsystem extends PIDSubsystem implements Loggable {
     @Log private final CANSparkMax shooterLeader = new CANSparkMax(1, CANSparkMaxLowLevel.MotorType.kBrushless);
     @Log private final CANSparkMax shooterFollower = new CANSparkMax(14, CANSparkMaxLowLevel.MotorType.kBrushless);
     @Log private final CANEncoder shooterEncoder = shooterLeader.getEncoder();
--- src/main/java/com/team4099/falconcamps/subsystems/ShooterSparkPID.java
+++ src/main/java/com/team4099/falconcamps/subsystems/ShooterSparkPID.java
@@ -2,9 +2,10 @@ package com.team4099.falconcamps.subsystems;
 
 import com.revrobotics.*;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import io.github.oblarg.oblog.Loggable;
 import io.github.oblarg.oblog.annotations.Log;
 
-public class ShooterSparkPID extends SubsystemBase {
+public class ShooterSparkPID extends SubsystemBase implements Loggable {
     @Log private final CANSparkMax shooterLeader = new CANSparkMax(1, CANSparkMaxLowLevel.MotorType.kBrushless);
     @Log private final CANSparkMax shooterFollower = new CANSparkMax(14, CANSparkMaxLowLevel.MotorType.kBrushless);
     @Log private final CANEncoder shooterEncoder = shooterLeader.getEncoder();
